21818851af7e5adc68b829b4ef7ba9e1e88a5c46
Vaibhavibhalke <vaibhavibhalke03@gmail.com>
2024-06-24 19:24:29 +0530

    Initial commit

---
+++ server/database/db.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const Connection = () => {
+
+    const MONGODB_URI = 'mongodb://127.0.0.1:27017';
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
